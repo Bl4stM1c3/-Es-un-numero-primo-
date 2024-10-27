ed66548c1dc9a50037e4eacfb126ad6f86e43cb8
Bl4stM1c3 <119587305+Bl4stM1c3@users.noreply.github.com>
2024-10-27 11:51:30 +0100

    Create es_primo.py
    solucion ejercicio 4 de https://retosdeprogramacion.com/ejercicios/

---
+++ es_primo.py
@@ -0,0 +1,21 @@
+#Esta es una posible solucion en python al ejercicio nº 4 de https://retosdeprogramacion.com/ejercicios/ 
+
+
+#Creamos una funcion que identifique si el numero es primo o no
+def es_primo(numero):
+  if numero < 2:
+    return False
+  elif numero == 2 or numero == 3:
+    return True
+  elif numero % 2 == 0 or numero % 3 == 0: #Eliminamos los multiplos de 2 y de 3 de la iteracion
+    return False
+
+  i = 5
+  while i * i <= numero:
+    if numero % i == 0 or numero % (i+2) == 0:
+      return False
+    i += 6
+  return True
+
+#Hecha la funcion que identifica si un numero es primo o no, mostramos los numero primos desde el 1 al 100
+print([n for n in range(101) if es_primo(n) == True])
